dd9ba66e3c7c0a946ea67f92965bc4132ec59ffc
Thomas Flynn <tomflynn98@hotmail.com>
2022-12-11 12:47:41 -0500

    XML for activity2 (Voice to Text)

---
+++ app/src/main/res/layout/activity_2.java
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="utf-8"?>
+<LinearLayout
+        xmlns:android="http://schemas.android.com/apk/res/android"
+        xmlns:tools="http://schemas.android.com/tools"
+        xmlns:app="http://schemas.android.com/apk/res-auto"
+        android:layout_width="match_parent"
+        android:layout_height="match_parent"
+        tools:context=".Activity2"
+        android:gravity="center"
+        android:orientation="vertical">
+
+    <TextView
+            android:id="@+id/tvText"
+            android:layout_width="wrap_content"
+            android:layout_height="wrap_content"
+            android:text=""
+            android:textSize="18sp"
+            android:layout_marginBottom="10dp"/>
+    <ImageButton
+            android:id="@+id/btnSpeak"
+            android:layout_width="155dp"
+            android:layout_height="137dp"
+            android:background="@drawable/v2t"/>
+
+
+</LinearLayout>
